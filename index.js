58c966be3d3cf927ac43b3f81b4aa9f84a801b67
teralove <terascripts@gmail.com>
2018-05-04 05:22:13 -0400

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const Command = require('command');
+
+module.exports = function WhoTriedInvitingMe(dispatch) {
+    const command = Command(dispatch);
+    
+    dispatch.hook('S_BEGIN_THROUGH_ARBITER_CONTRACT', 1, (event) => {
+        if (event.type === 4) {
+            let timeNow = new Date();
+            let timeText = timeNow.getHours().toLocaleString(undefined, {minimumIntegerDigits: 2}) + ':' +
+                timeNow.getMinutes().toLocaleString(undefined, {minimumIntegerDigits: 2}) + ':' + 
+                timeNow.getSeconds().toLocaleString(undefined, {minimumIntegerDigits: 2});
+
+            command.message(' ' + event.sender + ' sent you a party invite at ' + timeText);
+        }
+    })
+}
