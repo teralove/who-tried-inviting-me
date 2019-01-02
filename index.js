1e4370ed143fd1ce82e272bd109a4e4ca238b9ff
Tera Coder <31596711+TerableCoder@users.noreply.github.com>
2019-01-02 07:08:34 -0500

    better fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function WhoTriedInvitingMe(dispatch) {
-    
+    const command = dispatch.command || dispatch.require.command;
     dispatch.hook('S_BEGIN_THROUGH_ARBITER_CONTRACT', 1, (event) => {
         if (event.type === 4) {
             let timeNow = new Date();
@@ -7,7 +7,7 @@ module.exports = function WhoTriedInvitingMe(dispatch) {
                 timeNow.getMinutes().toLocaleString(undefined, {minimumIntegerDigits: 2}) + ':' + 
                 timeNow.getSeconds().toLocaleString(undefined, {minimumIntegerDigits: 2});
 
-            dispatch.command.message(' ' + event.sender + ' sent you a party invite at ' + timeText);
+            command.message(' ' + event.sender + ' sent you a party invite at ' + timeText);
         }
     })
 }
